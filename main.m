4126311da3e3fc4f88bc8ea76ab600140bfb819c
Ce Gao <gaoce@coe.neu.edu>
2013-12-02 16:46:13 -0500

    improve main.m and file structure

--- main.m
+++ main.m
@@ -1,3 +1,9 @@
-addpath('./data');                      % data.mat
+%% data loading
+clear;clc;close all;
+
 addpath('./lib/nway331/');              % PARAFAC
-addpath('./lib/tensor_toolbox_2.5/');   % tensor toolbox
+% addpath('./lib/tensor_toolbox_2.5/');   % tensor toolbox
+
+% please manually run decomp_parafac_test.m before loading data 
+load ./data/data.mat;
+
