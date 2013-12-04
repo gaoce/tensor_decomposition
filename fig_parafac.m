f543ae737d790982ce99334f31e360bdc721784e
Ce Gao <gaoce@coe.neu.edu>
2013-12-04 14:24:45 -0500

    change file name figHeatMapDecomp to fig_heatMapDecomp

--- fig_parafac.m
+++ fig_parafac.m
@@ -2,5 +2,5 @@ nChem = length(unique(chemName));
 
 % heat maps
 for i = 1:nChem
-    figHeatMapDecomp(data(:,:,(18*i-17):(18*i)), factsCP{i}, chemName{18*i});
+    fig_heatMapDecomp(data(:,:,(18*i-17):(18*i)), factsCP{i}, chemName{18*i});
 end
