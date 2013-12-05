13e0b508085a40fe454ef004053de198d999f2d4
Ce Gao <gaoce@coe.neu.edu>
2013-12-05 15:55:53 -0500

    revise component num determination, including change result folder name

--- decomp_parafac_num.m
+++ decomp_parafac_num.m
@@ -1,21 +1,21 @@
 %% determine the number of components 
 % manually change _chem_ and fill in the vector in the next cell
-close all;
+close all;clc;
 
 chem    = 24;
 nIter   = 5;
-nFacMax = 10;
+nFacMax = 5;
 
-pftest(nIter, data(:,:,(18*chem-17):(18*chem)), nFacMax);
+pftest(nIter, data(:,:,(18*chem-17):(18*chem)), nFacMax,[0 0 0 0 NaN]);
 % save evidence
-saveas(gcf,['./fig/parafac/test/',chemName{18*chem},'.pdf']);
+saveas(gcf,['./fig/parafac/num/',chemName{18*chem},'.pdf']);
 %%
 % the following vector was created by Ce Gao on 2013-12-03
-nFac = [5 3 4 4 3 ...
-        2 4 4 2 3 ...
-        3 3 1 2 2 ...
-        2 4 4 3 2 ...
-        2 3 2 2]' ;
+nFac = [2 2 1 2 1 ...
+        2 1 2 2 2 ...
+        2 2 2 2 2 ...
+        2 2 2 1 2 ...
+        2 2 2 2]' ;
     
 %% put nFac into data.mat
 save ./data/data.mat data chemName geneName pathName nFac;
